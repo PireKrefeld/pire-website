2968b96783f7876d84ddc589a7c679266cfd6c43
PireKrefeld <piresolidarity@tuta.com>
2026-03-15 10:44:53 +0100

    bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import keystatic from '@keystatic/astro';
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 export default defineConfig({
+  // 🚀 DER MAGISCHE SCHALTER: Wir zwingen Astro, reines HTML zu bauen!
+  output: 'static', 
+  
   integrations: [
     react(),
     keystatic(),
-    markdoc()
-  ],
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    tailwind()
+  ]
 });
